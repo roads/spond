83de0e3bba0bf0d0586067dc85c039d43cc52bd1
Kaarina Aho <48125488+kaarinaaho@users.noreply.github.com>
2020-10-28 11:50:00 +0000

    Update losses.py

--- spond/losses.py
+++ spond/losses.py
@@ -55,3 +55,86 @@ def set_loss_func(fx, gmm_y_samples, gmm_scale):
     # Negative log-likelihood
     neg_ll = - torch.mean(gmm_fx.log_prob(gmm_y_samples), axis=0)
     return neg_ll
+
+
+def cycle_loss_flex(X, gf_x, Y=None, fg_y=None, loss_cycle_scale=1):
+    """
+    Calculate cycle consistency loss for a system and its mapping back
+    to itself through the model (l1 norm of distances between points)
+
+    Args:
+     - X: Original system, tensor
+     - gf_x: Resulting system for comparison to original. Tensor with 
+     same shape as X. Assumes points correspond to those in X
+     - Y and gf_y (optional): Second system
+
+    Output:
+     - tot_loss: cycle loss per concept
+    """
+
+    if Y == None:
+        loss = loss_cycle_scale * _euclidean_distance(X, gf_x)
+
+    elif Y is not None and fg_y is not None:
+
+        loss = (loss_cycle_scale * 0.5 
+                * _euclidean_distance(X, gf_x) + _euclidean_distance(Y, fg_y)
+                )
+
+    return loss
+
+
+def create_gmm(system, gmm_scale=0.05):
+
+    """
+    Generate probability distribution using gaussian kernels on a
+    system of points
+
+    Args:
+     - system: set of points from which gmm will be produced
+     - batches: bool indicating if system shape includes batch dimension
+     - kernel_size: stdev of kernel placed on each point to form gmm
+
+    Output: 
+     - gmm_x: gmm probability distribution
+    """
+
+    system = torch.squeeze(system)
+    n_dim = system.shape[-1]
+    n_concepts = system.shape[-2]
+
+    # Weight concepts equally
+    mix = D.Categorical(torch.ones(n_concepts,))
+    
+    # Covariance matrix (diagonal) set with gmm_scale
+    components = D.Independent(D.Normal(system, gmm_scale * torch.ones(n_dim,)), 1)
+    gmm_X = D.mixture_same_family.MixtureSameFamily(mix, components)
+    
+    return gmm_X
+
+
+def negloglik(dist, sample, dist_loss_scale):
+    """
+    Calculate loglikelihood of drawing a sample from a probability
+    distribution
+
+    Args:
+     - dist: probability distribution (e.g, output of create_gmm)
+    """
+    result = -torch.mean(dist.log_prob(sample.double()), axis = 0)
+    return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
