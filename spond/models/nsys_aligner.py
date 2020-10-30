5569b69de7e3e965018562e8ae3b2abe0a2b9071
Brett D. Roads <brett.roads@gmail.com>
2020-10-30 15:46:17 +0000

    Remove pandas import.

--- spond/models/nsys_aligner.py
+++ spond/models/nsys_aligner.py
@@ -15,8 +15,7 @@
 # limitations under the License.
 # ============================================================================
 
-import numpy as np 
-import pandas as pd
+import numpy as np
 
 import torch
 import torch.nn as nn
