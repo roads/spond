b5b0522c939b9a212bf226b4aec10e62c8ddcacc
PC <corno@alum.mit.edu>
2021-01-18 13:37:44 +0000

    Refactored into functions

--- spond/experimental/openimage/readfile.py
+++ spond/experimental/openimage/readfile.py
@@ -5,104 +5,121 @@ import time
 import subprocess
 import itertools
 import scipy.sparse
-
-# Download from https://storage.googleapis.com/openimages/v6/oidv6-train-annotations-human-imagelabels.csv
-# The file is large, so not included in source control
-fn = "oidv6-train-annotations-human-imagelabels.csv"
-#fn = "oidv6-train-annotations-bbox.csv"
-
-# Download from https://storage.googleapis.com/openimages/v6/oidv6-class-descriptions.csv
-labelsfn = 'oidv6-class-descriptions.csv'
-
-# Download from https://storage.googleapis.com/openimages/v6/oidv6-train-images-with-labels-with-rotation.csv
-imgfn = 'oidv6-train-images-with-labels-with-rotation.csv'
-
-# load the labels into a big dictionary: keys = labels, values = index
-# TODO: How will we deal with label changes?
-
-labels = {}
-start = time.time()
-with open(labelsfn) as fh:
-    # this file has no header so the first item is 0
-    for idx, line in enumerate(fh.readlines()):
-        # label,objectname - maybe do something else with objectname later
-        label = line.strip().split(",")[0]
-        labels[label] = idx
-end = time.time()
-total = end-start
-
-print(f"Done processing labels in {total} s")
-
-images = {}
-start = time.time()
-with open(imgfn) as fh:
-    for idx, line in enumerate(fh.readlines()):
-        if idx == 0:
-            continue # ignore the header
-        # imgname is the only field we care about
-        imgname = line.strip().split(",")[0]
-        images[imgname] = idx - 1  # line 1 in the file is the header
-end = time.time()
-total = end-start
-
-print(f"Done processing images in {total} s")
+import os
 
 
-out = subprocess.run(["wc", "-l", fn], capture_output=True)
+def timed(f):
+    # This decorator runs the decorated function and reports
+    # how long it took
+    def f_(*args, **kwargs):
+        start = time.time()
+        ret = f(*args, **kwargs)
+        end = time.time()
+        total = end-start
+        print(f"Ran {f.__name__} in {total} s")
+        return ret
 
-lines = int(out.stdout.decode().split(" ")[0])
+    return f_
+
+
+
+@timed
+def readlabels(labelsfn, rootdir='.'):
+    # load the labels into a big dictionary: keys = labels, values = index
+    # TODO: How will we deal with label changes?
+    labels = {}
+    fn = os.path.join(rootdir, labelsfn)
+    with open(fn) as fh:
+        # this file has no header so the first item is 0
+        for idx, line in enumerate(fh):
+            # label,objectname - maybe do something else with objectname later
+            label = line.strip().split(",")[0]
+            labels[label] = idx
+    return labels
+
+
+@timed
+def readimgs(imgfn, rootdir='.'):
+    # load the images into a big dictionary: keys = image name, values = index
+    # TODO: How will we deal with label changes?
+    images = {}
+    fn = os.path.join(rootdir, imgfn)
+    with open(fn) as fh:
+        for idx, line in enumerate(fh):
+            if idx == 0:
+                continue # ignore the header
+            # imgname is the only field we care about
+            imgname = line.strip().split(",")[0]
+            images[imgname] = idx - 1  # line 1 in the file is the header
+    return images
+
 
 # fields we care about for now are:
 # ImageID (position 0)
 # LabelName (position 2)
 # Confidence (position 3)
 
-
-def process(args):
-    # args: tuple of (filename, start line number, end line number)
-    # necessary for multiprocessing later.
-    filename, start, end = args
-    fh = open(filename, 'r')
+@timed
+def generate_counts(filename, labels, images, use_confidence=False, rootdir='.'):
+    # filename: File containing the mapping of images to labels
+    # labels: Dictionary of label names to label index, 0 indexed
+    # images: Dictionary of image names to image index, 0 indexed
+    fh = open(os.path.join(rootdir, filename), 'r')
     # convert to dictionary of keys scipy sparse matrix
     # store as dictionary with keys = tuples of image, label
     # values = count
     # this will be easier to convert to sparse matrix format later
     imglabels = {}
-    output = {}
-    for line in itertools.islice(fh, start, end):
+    for idx, line in enumerate(fh):
+        # ignore line 0 which is the header
+        if not idx:
+            continue
         vals = line.split(",")
         imgname, _, labelname, confidence = vals[:4]
         labelidx = labels[labelname]
         imgidx = images[imgname]
-        confidence = float(confidence)
-        # we may want to do something else other than increment by 1
-        # and also take into account confidence.
         imglabels.setdefault((imgidx, labelidx), 0)
-        imglabels[imgidx, labelidx] += 1
+        score = 1    # if it was found in the file at all
+        if use_confidence:
+            confidence = float(confidence)
+            score *= confidence
+        imglabels[imgidx, labelidx] += score
     fh.close()
-    return imglabels, output
+    return imglabels
 
 
-start = time.time()
-# ignore the header, which is in line 1
-imglabels, output = process((fn, 1, lines))
-end = time.time()
-total = end-start
+def co_occurrence(imglabels):
+    # imglabels: Dictionary of (image index, label index) to score
+    sorted_keys = sorted(imglabels.keys())
+    data = np.fromiter((imglabels[k] for k in sorted_keys),
+                       dtype=float, count=len(sorted_keys))
+    skarr = np.array(sorted_keys).T
+    imglabels_sparse = scipy.sparse.coo_matrix((data, (skarr[0], skarr[1])))
+    i = torch.LongTensor(skarr)
+    v = torch.FloatTensor(data)
+    imglabels_torch = torch.sparse.FloatTensor(i, v)
+    return imglabels_torch, imglabels_sparse
 
-# Rows: image index
-# Columns: label index
-Nimages = len(images)
-Nlabels = len(labels)
 
-# convert to scipy.sparse.coo_matrix
-# coo_matrix needs: array of data, (array of rows, array of columns)
-sorted_keys = sorted(imglabels.keys())
-data = np.fromiter((imglabels[k] for k in sorted_keys),
-                   dtype=float, count=len(sorted_keys))
-skarr = np.array(sorted_keys).T
-imglabels_sparse = scipy.sparse.coo_matrix((data, (skarr[0], skarr[1])))
-print(f"Done processing associations in {total} s")
-i = torch.LongTensor(skarr)
-v = torch.FloatTensor(data)
-imglabels_torch = torch.sparse.FloatTensor(i, v)
-# torch.save(imglabels_torch, 'image_labels.pt')
+
+if __name__ == '__main__':
+    #rootdir = "/home/petra/data"
+    rootdir = '.'
+
+    # Download from https://storage.googleapis.com/openimages/v6/oidv6-train-annotations-human-imagelabels.csv
+    # The file is large, so not included in source control
+    #fn = os.path.join(rootdir, "oidv6-train-annotations-human-imagelabels.csv")
+    fn = "oidv6-train-annotations-bbox.csv"
+
+    # Download from https://storage.googleapis.com/openimages/v6/oidv6-class-descriptions.csv
+    labelsfn = 'oidv6-class-descriptions.csv'
+
+    # Download from https://storage.googleapis.com/openimages/v6/oidv6-train-images-with-labels-with-rotation.csv
+    imgfn = 'oidv6-train-images-with-labels-with-rotation.csv'
+
+
+    labels = readlabels(labelsfn, rootdir=rootdir)
+    images = readimgs(imgfn, rootdir=rootdir)
+    imglabels = generate_counts(fn, labels, images, rootdir=rootdir)
+    coo_pt, coo_scipy = co_occurrence(imglabels)
+
